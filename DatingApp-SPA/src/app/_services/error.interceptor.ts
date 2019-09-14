f9fbe7ac74bf8fdec91ca49c9107b013bc261b05
sejdiuh <herolindsejdiu94@gmail.com>
2019-09-14 10:17:49 +0200

    error section

---
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpEvent, HttpHandler, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor{
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+         return next.handle(req).pipe(
+             catchError(error => {
+                 if (error instanceof HttpErrorResponse){
+
+                    if (error.status === 401) {
+                        return throwError(error.statusText);
+                    }
+                    const appError = error.headers.get('Application-Error');
+                    if (appError) {
+                        return throwError(appError);
+                    }
+                 }
+                 const serverError = error.error.error;
+                 let modalStateError = '';
+                 if (serverError && typeof serverError === 'object') {
+                    for (const key in serverError) {
+                        if (serverError[key]) {
+                            modalStateError +=  serverError[key] + '\n';
+                        }
+                    }
+                 }
+
+                 return throwError(modalStateError || serverError || 'Server Error');
+          })
+        );
+    }
+}
+
+export const ErrorInterceptorProvider = {
+    provide: HTTP_INTERCEPTORS,
+    useClass: ErrorInterceptor,
+    multi: true
+}
